94b7d4d1625d70e2c34cbf20cd6950bbcf3ee545
EugeneP <eugene@lightsonsoftware.com>
2024-06-12 23:33:18 +0200

    TLK-580 test commit

--- .github/scripts/run_docker_migration.sh
+++ .github/scripts/run_docker_migration.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd $SSH_PROJECT_DIR
-git pull origin TLK-580_automated_migrations
+git pull
 make migrate
 echo "Migration complete"
--- .github/scripts/run_src_migration.sh
+++ .github/scripts/run_src_migration.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd $SSH_PROJECT_DIR
-git pull origin TLK-580_automated_migrations
+git pull
 source .venv/bin/activate
 alembic -c src/backend/alembic.ini upgrade head
 echo "Migration complete"
