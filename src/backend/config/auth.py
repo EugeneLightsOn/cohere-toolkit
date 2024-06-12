761e421dffb11215a9e2b2b801ce62652386e250
EugeneP <eugene@lightsonsoftware.com>
2024-06-13 00:31:52 +0200

    TLK-580 test commit

--- src/backend/config/auth.py
+++ src/backend/config/auth.py
@@ -1,4 +1,6 @@
-from backend.services.auth import BasicAuthentication, GoogleOAuth
+import os
+from fastapi import HTTPException, Depends, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 # Add Auth strategy classes here to enable them
 # Ex: [BasicAuthentication]
@@ -8,6 +10,22 @@ ENABLED_AUTH_STRATEGIES = []
 # During runtime, this will create an instance of each enabled strategy class.
 # Ex: {"Basic": BasicAuthentication()}
 ENABLED_AUTH_STRATEGY_MAPPING = {cls.NAME: cls() for cls in ENABLED_AUTH_STRATEGIES}
+
+# Token to authorize migration requests
+MIGRATE_TOKEN = os.environ.get(
+    "MIGRATE_TOKEN", None
+)
+
+security = HTTPBearer()
+
+
+def verify_migrate_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    if credentials.credentials != MIGRATE_TOKEN:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or missing token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
 
 
 def is_authentication_enabled() -> bool:
